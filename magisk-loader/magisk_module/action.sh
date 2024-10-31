4001be6cc56dc470002b9dd211ce72848a3fcbcc
Keeperorowner <qingfong0731@outlook.com>
2024-10-31 22:02:28 +0800

    action.sh: use a different method to open manager
    Co-Authored-By: JingMatrix <jingmatrix@gmail.com>

--- magisk-loader/magisk_module/action.sh
+++ magisk-loader/magisk_module/action.sh
@@ -1,6 +1,4 @@
-API=$(getprop ro.build.version.sdk)
-if [ "$API" -ge 29 ]; then
-  am broadcast -a android.telephony.action.SECRET_CODE -d android_secret_code://5776733 android
-else
-  am broadcast -a android.provider.Telephony.SECRET_CODE -d android_secret_code://5776733 android
-fi
+DEFAULT_MANAGER_PACKAGE_NAME=org.lsposed.manager
+INJECTED_PACKAGE_NAME=com.android.shell
+
+am start -c "${DEFAULT_MANAGER_PACKAGE_NAME}.LAUNCH_MANAGER" "${INJECTED_PACKAGE_NAME}/.BugreportWarningActivity"
