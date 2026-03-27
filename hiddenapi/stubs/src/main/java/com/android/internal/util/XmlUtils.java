abce0715ec5eeb14e689b84cf23133c8e47c0480
frknkrc44 <krc440002@gmail.com>
2026-03-27 13:19:32 +0300

    Try to fix Android 17 support

--- hiddenapi/stubs/src/main/java/com/android/internal/util/XmlUtils.java
+++ hiddenapi/stubs/src/main/java/com/android/internal/util/XmlUtils.java
@@ -1,5 +1,7 @@
 package com.android.internal.util;
 
+import com.android.modules.utils.TypedXmlPullParser;
+
 import org.xmlpull.v1.XmlPullParserException;
 
 import java.io.IOException;
@@ -9,6 +11,10 @@ import java.util.HashMap;
 public class XmlUtils {
 	@SuppressWarnings("rawtypes")
 	public static final HashMap readMapXml(InputStream in) throws XmlPullParserException, IOException {
+		throw new UnsupportedOperationException("STUB");
+	}
+
+	public static final Object readValueXml(TypedXmlPullParser parser, String[] name) throws XmlPullParserException, IOException {
 		throw new UnsupportedOperationException("STUB");
 	}
 }
---
+++ hiddenapi/stubs/src/main/java/com/android/modules/utils/TypedXmlPullParser.java
@@ -0,0 +1,6 @@
+package com.android.modules.utils;
+
+import org.xmlpull.v1.XmlPullParser;
+
+public class TypedXmlPullParser extends XmlPullParser {
+}
